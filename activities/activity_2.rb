9ceea5466ed11ba66d65685d957727ca592a3e3c
vpjuguilon <juguilon.vincepaul@gmail.com>
2024-02-16 15:53:57 +0800

    activity 2: function that writes the full anme

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -6,9 +6,13 @@
 hash_name = {first_name: 'Vince', middle_name:'Paul', last_name:'Juguilon'}
 
 def full_name(hash_name)
+
+  full_name = []
   hash_name.each do |key, value|
-    print value, " "
+    full_name.append(value)
+    full_name.append(" ")
   end
+  print(full_name.join)
 end
 
 full_name(hash_name)
