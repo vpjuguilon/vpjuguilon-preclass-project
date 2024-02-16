f508918f7d3e8772f4175abd2760cf03a5554533
vpjuguilon <juguilon.vincepaul@gmail.com>
2024-02-16 14:31:24 +0800

    activity 1: function which takes the average age of the list of ages

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,9 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+
+def ave_age(age_list)
+  puts age_list.sum / age_list.size
+end
+
+#ave_age([5, 10, 15, 20, 25])
